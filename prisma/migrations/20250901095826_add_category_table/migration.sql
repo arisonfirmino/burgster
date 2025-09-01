74efb0e1b4ab579737bc52479ef06adffb75bcd9
arisonfirmino <firminoarison16@gmail.com>
2025-09-01 06:59:17 -0300

    feat: adiciona tabela Category no schema do Prisma e realiza migrate

---
+++ prisma/migrations/20250901095826_add_category_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "public"."categories" (
+    "_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("_id")
+);
